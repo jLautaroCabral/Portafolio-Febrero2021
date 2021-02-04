4e4b712aa3c085d6388c0b28671567e8e4880af6
Lautaro Cabral <lautaro.cabral.dev@gmail.com>
2021-02-03 21:25:06 -0300

    Probando matterJS y p5

---
+++ sketch.js
@@ -0,0 +1,57 @@
+// module aliases
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine;
+var world;
+var boxes = [];
+var groud;
+function setup() {
+    createCanvas(400, 400);
+    engine = Engine.create();
+    world = engine.world;
+
+    Engine.run(engine);
+
+    groud = Bodies.rectangle(200, height, width, 100, { isStatic: true});
+    
+    World.add(world, groud);
+}
+
+function mouseDragged() {
+    boxes.push(new Box(mouseX, mouseY, 20, 20));
+}
+
+function draw() {
+    background(51);
+    rect(0, 0, groud.h, groud.h);
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+    }
+}
+
+ 
+/*
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+// create two boxes and a ground
+
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
+*/
